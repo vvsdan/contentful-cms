01001a24e0a7ab7c2861dc99a606c28be0e1addc
Daniel Jones <vvsdan999@gmail.com>
2023-10-30 16:58:08 -0400

    update data links to projects hosted by dan

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 const projects = [
   {
     title: "birthday buddy",
-    url: "https://react-vite-projects-1-birthday-buddy.netlify.app/",
+    url: "https://elaborate-cat-74a697.netlify.app",
     image: "./assets/birthday.png",
   },
   {
     title: "tours",
-    url: "https://react-vite-projects-2-tours.netlify.app/",
+    url: "https://candid-marigold-bb08c2.netlify.app",
     image: "./assets/tours.png",
   },
   {
     title: "reviews",
-    url: "https://react-vite-projects-3-reviews.netlify.app/",
+    url: "https://timely-biscochitos-4753b1.netlify.app",
     image: "./assets/reviews.png",
   },
   {
     title: "questions",
-    url: "https://react-vite-projects-4-accordion.netlify.app/",
+    url: "https://earnest-meerkat-ec1610.netlify.app",
     image: "./assets/questions.png",
   },
 ];
